865a2cc8a321245ebc5ed087dc628cb56a6d8f89
Muhammad Hammad <hammadiftikhar263@gmail.com>
2025-07-25 18:31:43 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Content from "./Components/content"
+import Content from "./Components/Content"
 import Aboutme from './Components/Aboutme'
 import Teac from './Components/Teac'
 import Experience from "./Components/Experience"
