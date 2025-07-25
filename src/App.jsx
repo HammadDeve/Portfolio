fc97930248b23960a777552c3586a8b0cff54204
Muhammad Hammad <hammadiftikhar263@gmail.com>
2025-07-25 18:24:57 +0500

    Update App.jsx
    Just 3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Content from "./Components/content"
 import Aboutme from './Components/Aboutme'
 import Teac from './Components/Teac'
